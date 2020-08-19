181b81faa624c09880ca0e73ef3b0fbee263c72f
MrClapton <f1nk121@yandex.ru>
2020-08-19 16:25:29 +0300

    Домашнее задание №1 без комментариев

--- homeworkOne/src/main/java/ru/geekbrains/java/homework/homeworkOne.java
+++ homeworkOne/src/main/java/ru/geekbrains/java/homework/homeworkOne.java
@@ -12,22 +12,22 @@ public class homeworkOne {
         char charVal = '\u2256' + '\u2256'; //Можно ли вывести в строку 2 символа кодировки? У меня при сложении их как строк получается иероглиф, а не '=='.
         boolean booleanVal = false;
 
-        //System.out.println(charVal);
+        System.out.println(charVal);
 
-        //System.out.println(thirdTask(0.4f,1.24f,10.26f,2.0f));
+        System.out.println(thirdTask(0.4f,1.24f,10.26f,2.0f));
 
-        //System.out.println(fourthTask(10,2));     //true
-        //System.out.println(fourthTask(10,22));    //false
+        System.out.println(fourthTask(10,2));     //true
+        System.out.println(fourthTask(10,22));    //false
 
-        //fifthTask(10);  //"Получено положительное число"
-        //fifthTask(-5); //"Получено отрицательное число"
+        fifthTask(10);  //"Получено положительное число"
+        fifthTask(-5); //"Получено отрицательное число"
 
-        //System.out.println(sixthTask(15));  //false
-        //System.out.println(sixthTask(-20));  //true
+        System.out.println(sixthTask(15));  //false
+        System.out.println(sixthTask(-20));  //true
 
-        //seventhTask("Алекс");   //"Привет, Алекс!"
+        seventhTask("Алекс");   //"Привет, Алекс!"
 
-        //eighthTask(1486);   //Полученный 1486 год не является високосным!
+        eighthTask(1486);   //Полученный 1486 год не является високосным!
 
     }
 
